fd6540e7e8baf9e0d24645467503bb54a3221021
sakshideshmukh230-web <sakshideshmukh230@gmail.com>
2025-10-16 12:15:03 +0530

    Cibil Calculator project Created and added by Sakshi

---
+++ cibilcalculator/src/test/java/in/nexa/cibilcalculator/app/CibilcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nexa.cibilcalculator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibilcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
