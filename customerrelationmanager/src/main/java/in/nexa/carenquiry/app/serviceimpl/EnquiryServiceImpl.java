99812798e147b1489566f556d8fdce4f87fd754a
sakshideshmukh230-web <sakshideshmukh230@gmail.com>
2025-10-25 12:23:26 +0530

    forwardToOe Api is created in Enquiry Module

--- customerrelationmanager/src/main/java/in/nexa/carenquiry/app/serviceimpl/EnquiryServiceImpl.java
+++ customerrelationmanager/src/main/java/in/nexa/carenquiry/app/serviceimpl/EnquiryServiceImpl.java
@@ -1,10 +1,10 @@
 package in.nexa.carenquiry.app.serviceimpl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import in.nexa.carenquiry.app.model.Enquiry;
 import in.nexa.carenquiry.app.repository.EnquiryRepo;
 import in.nexa.carenquiry.app.servicei.EnquiryServiceI;
@@ -14,9 +14,10 @@ public class EnquiryServiceImpl implements EnquiryServiceI {
 	
 	@Autowired
 	EnquiryRepo er;
-
+	
 	@Override
 	public Enquiry saveAll(Enquiry enquiry) {
+		enquiry.getCibil().setCibilscoredDateTime(new Date());
 		return er.save(enquiry);
 	}
     
@@ -42,6 +43,12 @@ public class EnquiryServiceImpl implements EnquiryServiceI {
 	public List<Enquiry> getAllEnquiry() {
 		return er.findAll(); 
 	}
-	
+
+	@Override
+	public Enquiry forwardedToOe(int customerId) {
+		Enquiry e = er.findById(customerId).get();
+		e.setEnquiryStatus("forwardedtooe");
+		return er.save(e);
+	}
 
 }
